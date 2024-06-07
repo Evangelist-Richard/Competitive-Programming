46d8e6a126f88372cec056fcb95eb5e7c3eabea5
Evangelist Richard Gomes <124143134+Evangelist-Richard@users.noreply.github.com>
2024-06-07 09:43:21 +0600

    Add files via upload

---
+++ 32B Borze.cpp
@@ -0,0 +1,34 @@
+// https://codeforces.com/problemset/problem/32/B
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    for(int i=0; i<s.length(); )
+    {
+        if(s[i] == '.')
+        {
+            cout << "0";
+            i++;
+        }
+
+        else if(s[i] == '-' && s[i+1] == '.')
+        {
+            cout << "1";
+            i += 2;
+        }
+
+        else if(s[i] == '-' && s[i+1] == '-')
+        {
+            cout << "2";
+            i += 2;
+        }
+    }
+    cout << endl;
+    return 0;
+}
