1f542b4bba3d46506adcc4345e0ca0a8260288b2
Evangelist Richard Gomes <124143134+Evangelist-Richard@users.noreply.github.com>
2024-06-09 21:37:39 +0600

    Add files via upload

---
+++ Codeforces-Solution/50A Domino_Piling.cpp
@@ -0,0 +1,16 @@
+// https://codeforces.com/problemset/problem/50/A
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, area;
+    cin >> a >> b;
+
+    area = a*b;
+
+    cout << area/2 << endl;
+    return 0;
+}
