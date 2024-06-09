ae7d167d26e541e6ed484f25ee15db9a645dd764
Evangelist Richard Gomes <124143134+Evangelist-Richard@users.noreply.github.com>
2024-06-09 21:35:07 +0600

    Update 41A Translation.cpp

--- Codeforces-Solution/41A Translation.cpp
+++ Codeforces-Solution/41A Translation.cpp
@@ -1,1 +1,23 @@
+// https://codeforces.com/problemset/problem/41/A
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s, t;
+    int cnt=0;
+
+    cin >> s;
+    cin >> t;
+
+    for(int i=0; i<s.length(); i++)
+    {
+        if(s[i]==t[s.length()-1-i])
+            cnt++;
+    }
+
+    (cnt == s.length()) ? cout << "YES" << endl: cout << "NO" << endl;
+    return 0;
+}
