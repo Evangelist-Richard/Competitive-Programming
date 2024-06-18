d2bd964e9c3adc4962aa8b6f8605b675583eb08f
Evangelist Richard Gomes <124143134+Evangelist-Richard@users.noreply.github.com>
2024-06-18 12:11:04 +0600

    Update 61A Ultra-Fast Mathematician.cpp

--- Codeforces-Solution/61A Ultra-Fast Mathematician.cpp
+++ Codeforces-Solution/61A Ultra-Fast Mathematician.cpp
@@ -20,3 +20,18 @@ int main()
     cout << endl;
     return 0;
 }
+
+// ----->>Another Approach <<-----
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    for(int i=0; i<s1.length(); i++)
+        (s1[i] == s2[i]) ? cout << "0" : cout << "1";
+    return 0;
+}
+*/
