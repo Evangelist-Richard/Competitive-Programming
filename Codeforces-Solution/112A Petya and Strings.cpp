f582c8056331f6cc71e787d1a2cdad7a1984e7ce
Evangelist Richard Gomes <124143134+Evangelist-Richard@users.noreply.github.com>
2024-07-10 17:16:21 +0600

    Add files via upload

---
+++ Codeforces-Solution/112A Petya and Strings.cpp
@@ -0,0 +1,25 @@
+// 112A_Petya_and_Strings
+// https://codeforces.com/problemset/problem/112/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    transform(s1.begin(), s1.end(), s1.begin(), ::toupper);
+    transform(s2.begin(), s2.end(), s2.begin(), ::toupper);
+
+        if(s1 == s2)
+            cout << "0" << endl;
+        else if(s1 < s2)
+            cout << "-1" << endl;
+
+        else if(s1 > s2)
+            cout << "1" << endl;
+
+    return 0;
+}
