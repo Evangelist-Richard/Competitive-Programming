bd8a5538be8688d09c18caa2a7cf3801d771c704
Evangelist Richard Gomes <124143134+Evangelist-Richard@users.noreply.github.com>
2024-09-20 11:09:18 +0600

    Add files via upload

---
+++ Codeforces-Solution/115 A. Restoring Three Numbers.cpp
@@ -0,0 +1,17 @@
+// 115 A. Restoring Three Numbers
+// https://codeforces.com/problemset/problem/1154/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+
+    set <int> s = {a,b,c,d};
+
+    cout << s.size() << endl;
+    return 0;
+}
