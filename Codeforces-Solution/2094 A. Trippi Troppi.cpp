eeda4d4f187a1e5b80038fe05eeff8989d90aa01
Evangelist Richard Gomes <124143134+Evangelist-Richard@users.noreply.github.com>
2025-07-06 10:20:23 +0600

    Add files via upload

---
+++ Codeforces-Solution/2094 A. Trippi Troppi.cpp
@@ -0,0 +1,25 @@
+// 2094 A. Trippi Troppi
+// https://codeforces.com/contest/2094/problem/A
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int x = 3;
+        string s;
+
+        while(x--)
+        {
+            cin >> s;
+            cout << s[0];
+        }
+        cout << endl;
+    }
+    return 0;	
+}
